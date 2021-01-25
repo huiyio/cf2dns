34b6dff6975034d62fbf166b7649afc09f491151
回忆 <60065129+huiyio@users.noreply.github.com>
2021-01-25 18:41:57 +0800

    Update cf2dns.py

--- cf2dns.py
+++ cf2dns.py
@@ -20,9 +20,7 @@ KEY = "o1zrmHAF"
 
 #CM:移动 CU:联通 CT:电信
 #修改需要更改的dnspod域名核子域名
-DOMAINS = {
-    {"huiyiu.com": {"www": ["CM","CU","CT"]}
-}
+DOMAINS = {"huiyiu.com": {"@": ["CM","CU","CT"], "www": ["CM","CU","CT"]}}
 
 #解析生效条数 免费的DNSPod相同线路最多支持2条解析
 AFFECT_NUM = 2
